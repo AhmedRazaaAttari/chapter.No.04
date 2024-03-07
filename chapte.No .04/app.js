f33df6d77a29ad6a60875617fa25b3b56b4b6796
AhmedRazaaAttari <154536166+AhmedRazaaAttari@users.noreply.github.com>
2024-03-08 00:23:08 +0500

    Add files via upload

---
+++ chapte.No .04/app.js
@@ -0,0 +1,53 @@
+// 1. Declare 3 variables in one statement.
+
+var $star = "Hello world!";
+console.log($star);
+
+var _star = "how are you?";
+console.log(_star);
+
+var starWar = "I am fine.";
+console.log(starWar);
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// legal variable
+
+// var rougeOne; // contains letter;
+// var rouge1; // contains number;
+// var _rouge1; // contains  underscore;
+// var $_rouge1; // contains dollar sign;
+
+// illegal variable
+
+// var ; // begins with a number;
+// var ; // has a space between words;
+// var ; // contains special characters;
+// var ; // JavaScript reserved keyword class , do , if;
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+
+document.write("<h3>Rules for naming JS variables</h3>");
+
+var $my_1stVariable = "Hello world!</br>";
+document.write($my_1stVariable);
+
+var _my$Variable = "How are you?</br>";
+document.write(_my$Variable);
+
+var myVariable = "I am find</br>";
+document.write(myVariable);
+
+var Myvariable = "ok</br>";
+document.write(Myvariable);
+
+// c) Variables must begin with a $ster,_star or
+// sTar.
+// d) Variable names are case camel case
+// e) Variable names should not be JS class, do.
+
+// ChannelSplitterNode.No.04 complete
